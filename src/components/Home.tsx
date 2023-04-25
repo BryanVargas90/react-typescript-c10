87564c91d98195f66dcfd36b3691adee4ab6824a
bryandanielpsp@gmail.com <bryandanielpsp@gmail.com>
2023-04-25 17:11:12 -0400

    react with typescript

---
+++ src/components/Home.tsx
@@ -0,0 +1,16 @@
+
+
+interface homeProp {
+    name: string,
+    age: number
+}
+
+
+export default function
+Home ({name, age}: homeProp) {
+    return ( 
+        <>
+        home page {name} {age}
+        </>
+    )
+}
